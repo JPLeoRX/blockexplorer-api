88a4af5eaefe3a9b3452f2cd44b63e64d83cfa53
Leo Ertuna <leo.ertuna@gmail.com>
2018-06-09 13:32:03 +0300

    Model interface

---
+++ src/main/java/com/tekleo/blockexplorer_api/model/Model.java
@@ -0,0 +1,17 @@
+package com.tekleo.blockexplorer_api.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.Serializable;
+
+/**
+ * Model interface
+ *
+ * @author Leo Ertuna
+ * @since 09.06.2018 13:31
+ */
+interface Model extends Serializable {
+    public static final GsonBuilder GSON_BUILDER = new GsonBuilder();
+    public static final Gson GSON = GSON_BUILDER.create();
+}
